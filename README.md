a22e2d0ba8d5cfe4b86429f0328787ffbab92ac4
omareldars <omar.a.eldars@gmail.com>
2021-03-28 13:54:36 +0200

    First Lab-Shoping Cart

---
+++ README.md
@@ -0,0 +1,1 @@
+#My Vue.JS-Labs
---
+++ books.js
@@ -0,0 +1,63 @@
+books = [
+    {
+        ISBN : 100,
+        Name:"Land of OZ",
+        Category:"Science Fiction",
+        Image:'unnamed.png',
+        Pages:500,
+        Author:"Robert d. Wedie",
+        Price:100,
+        Quantity:4
+    },
+    {
+        ISBN : 200,
+        Name:"Django for All",
+        Category:"Information Technology",
+        Image:'unnamed.png',
+        Pages:600,
+        Author:"Hally Brawn",
+        Price:150,
+        Quantity:35
+    },
+    {
+        ISBN : 300,
+        Name:"Screw",
+        Category:"Horror",
+        Image:'unnamed.png',
+        Pages:350,
+        Author:"Kallen Stewart",
+        Price:200,
+        Quantity:25
+    },
+    {
+        ISBN : 400,
+        Name:"Art of Drawing",
+        Category:"Art",
+        Image:'unnamed.png',
+        Pages:400,
+        Author:"Van Gogh",
+        Price:330,
+        Quantity:40
+    },
+    {
+        ISBN : 500,
+        Name:"Prey",
+        Category:"Horror",
+        Image:'unnamed.png',
+        Pages:200,
+        Author:"Cristiano Ronaldo",
+        Price:600,
+        Quantity:20
+    },
+    {
+        ISBN : 600,
+        Name:"Playing Football",
+        Category:"Soccer",
+        Image:'unnamed.png',
+        Pages:1000,
+        Author:"M. A. Trieka",
+        Price:1000,
+        Quantity:0
+    },
+    
+]
